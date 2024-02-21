65c52b7b6f2d2a17e6ad57d8ce5f61ac14674b30
rabTAI <rabinofficial@hotmail.com>
2024-02-20 23:20:54 -0700

    updated help command

--- apps/discordBot/src/commands/lp.js
+++ apps/discordBot/src/commands/lp.js
@@ -1,3 +1,4 @@
+//Shows LP of two Tokens and $value of LP amounts
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const messageDisplay = require("../core/messageDisplay.js");
 
@@ -5,22 +6,23 @@ const messageDisplay = require("../core/messageDisplay.js");
 const discordData = new SlashCommandBuilder()
   .setName("lp")
   .setDescription("Displays LP price")
-    .addStringOption(option => 
-        option
-            .setName('first_token')
-                .setDescription('Provide First Token Symbol')
-                .setRequired(true))
+  .addStringOption((option) =>
+    option
+      .setName("first_token")
+      .setDescription("Provide First Token Symbol")
+      .setRequired(true)
+  )
 
-    .addStringOption(option => 
-        option
-            .setName('second_token')
-            .setDescription('Provide Second Token Symbol')
-            .setRequired(true))
+  .addStringOption((option) =>
+    option
+      .setName("second_token")
+      .setDescription("Provide Second Token Symbol")
+      .setRequired(true)
+  )
 
-    .addNumberOption(option => 
-        option
-            .setName('lp_amount')
-            .setDescription('Provide LP amount'))
+  .addNumberOption((option) =>
+    option.setName("lp_amount").setDescription("Provide LP amount")
+  );
 
 // Callback for discord
 const execute = async (interaction) => {
@@ -31,15 +33,22 @@ module.exports = { discordData, execute };
 
 //Command function
 async function lp(interaction) {
-  const first_token = interaction.options.getString('first_token')
-  const second_token=interaction.options.getString('second_token')
+  const first_token = interaction.options.getString("first_token");
+  const second_token = interaction.options.getString("second_token");
 
-    const lp_amount=interaction.options.getNumber('lp_amount')??'NoLpAmount'
+  const lp_amount = interaction.options.getNumber("lp_amount") ?? "NoLpAmount";
 
-    if(lp_amount=="NoLpAmount"){
-        await messageDisplay.success(interaction, `${first_token} ${second_token} LP`, "No LP amount");
-    }else{
-        await messageDisplay.success(interaction, `${first_token} ${second_token} LP`, "LP amount provided");
-
-    }
+  if (lp_amount == "NoLpAmount") {
+    await messageDisplay.success(
+      interaction,
+      `${first_token} ${second_token} LP`,
+      "No LP amount"
+    );
+  } else {
+    await messageDisplay.success(
+      interaction,
+      `${first_token} ${second_token} LP`,
+      "LP amount provided"
+    );
+  }
 }
