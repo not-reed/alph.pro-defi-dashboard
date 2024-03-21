3c0168e8c6defc1d4e7a44e74fb05ea63067bda8
not-reed <reed@degen.watch>
2024-03-20 20:16:32 -0700

    fix double count locked balances

--- apps/indexer/src/cli/balances.ts
+++ apps/indexer/src/cli/balances.ts
@@ -4,102 +4,102 @@ import { config } from "../config";
 import { addressFromContractId } from "@alephium/web3";
 import type { NewBalance } from "../database/schemas/public/Balance";
 import {
-	deleteUserBalances,
-	insertUserBalances,
+  deleteUserBalances,
+  insertUserBalances,
 } from "../database/services/balance";
 
 const nodeHeaders: Record<string, string> = { Accept: "application/json" };
 
 if (config.NODE_BASIC_AUTH) {
-	nodeHeaders.Authorization = `Basic ${config.NODE_BASIC_AUTH}`;
+  nodeHeaders.Authorization = `Basic ${config.NODE_BASIC_AUTH}`;
 }
 
 if (config.NODE_API_KEY) {
-	nodeHeaders["X-API-KEY"] = config.NODE_API_KEY;
+  nodeHeaders["X-API-KEY"] = config.NODE_API_KEY;
 }
 
 export async function fixBalances(opts: {
-	user: string;
-	min: number;
-	max: number;
-	force: boolean;
+  user: string;
+  min: number;
+  max: number;
+  force: boolean;
 }) {
-	let query = db
-		.selectFrom("Balance")
-		.select(["userAddress", (eb) => eb.fn.count("tokenAddress").as("count")])
-		.where("balance", "<>", 0n);
-	if (opts.user) {
-		query = query.where("Balance.userAddress", "=", opts.user);
-	}
-	query = query.groupBy("userAddress");
+  let query = db
+    .selectFrom("Balance")
+    .select(["userAddress", (eb) => eb.fn.count("tokenAddress").as("count")])
+    .where("balance", "<>", 0n);
+  if (opts.user) {
+    query = query.where("Balance.userAddress", "=", opts.user);
+  }
+  query = query.groupBy("userAddress");
 
-	if (opts.min) {
-		query = query.having(sql`count(distinct "tokenAddress")`, ">=", opts.min);
-	}
+  if (opts.min) {
+    query = query.having(sql`count(distinct "tokenAddress")`, ">=", opts.min);
+  }
 
-	if (opts.max) {
-		query = query.having(sql`count(distinct "tokenAddress")`, "<=", opts.max);
-	}
+  if (opts.max) {
+    query = query.having(sql`count(distinct "tokenAddress")`, "<=", opts.max);
+  }
 
-	const users = await query
-		.orderBy(sql`count(distinct "tokenAddress")`, "desc")
-		.execute();
+  const users = await query
+    .orderBy(sql`count(distinct "tokenAddress")`, "desc")
+    .execute();
 
-	console.log(`Found ${users.length} users with non-zero balances`);
+  console.log(`Found ${users.length} users with non-zero balances`);
 
-	for (const user of users) {
-		const balances = await fetch(
-			`${config.NODE_URL}/addresses/${user.userAddress}/balance`,
-			{ headers: nodeHeaders },
-		).then((a) => a.json());
+  for (const user of users) {
+    const balances = await fetch(
+      `${config.NODE_URL}/addresses/${user.userAddress}/balance`,
+      { headers: nodeHeaders }
+    ).then((a) => a.json());
 
-		const wallet = []
-			.concat(balances.tokenBalances ?? [])
-			.concat(balances.lockedTokenBalances ?? [])
-			.reduce(
-				(
-					acc: Map<string, bigint>,
-					cur: { id: string; amount: `${number}` },
-				) => {
-					try {
-						const address = addressFromContractId(cur.id);
-						const prev = acc.get(address);
-						if (prev) {
-							acc.set(address, prev + BigInt(cur.amount));
-						} else {
-							acc.set(address, BigInt(cur.amount));
-						}
-						return acc;
-					} catch (err) {
-						console.log({ cur, user });
-						throw err;
-					}
-				},
-				new Map<string, bigint>(),
-			);
+    const wallet = []
+      .concat(balances.tokenBalances ?? [])
+      .concat(balances.lockedTokenBalances ?? [])
+      .reduce(
+        (
+          acc: Map<string, bigint>,
+          cur: { id: string; amount: `${number}` }
+        ) => {
+          try {
+            const address = addressFromContractId(cur.id);
+            const prev = acc.get(address);
+            if (prev) {
+              acc.set(address, prev + BigInt(cur.amount));
+            } else {
+              acc.set(address, BigInt(cur.amount));
+            }
+            return acc;
+          } catch (err) {
+            console.log({ cur, user });
+            throw err;
+          }
+        },
+        new Map<string, bigint>()
+      );
 
-		wallet.set(
-			"tgx7VNFoP9DJiFMFgXXtafQZkUvyEdDHT9ryamHJYrjq",
-			BigInt(balances.balance || 0) + BigInt(balances.lockedBalance || 0),
-		);
+    wallet.set(
+      "tgx7VNFoP9DJiFMFgXXtafQZkUvyEdDHT9ryamHJYrjq",
+      BigInt(balances.balance || 0)
+    );
 
-		if (BigInt(wallet.size) === user.count && !opts.force) {
-			continue;
-		}
+    if (BigInt(wallet.size) === user.count && !opts.force) {
+      continue;
+    }
 
-		const newBalances: NewBalance[] = [];
-		for (const [addr, bal] of wallet) {
-			newBalances.push({
-				userAddress: user.userAddress,
-				tokenAddress: addr,
-				balance: bal,
-			});
-		}
-		await db.transaction().execute(async (trx) => {
-			await deleteUserBalances(user.userAddress, trx);
-			await insertUserBalances(newBalances, trx);
-		});
+    const newBalances: NewBalance[] = [];
+    for (const [addr, bal] of wallet) {
+      newBalances.push({
+        userAddress: user.userAddress,
+        tokenAddress: addr,
+        balance: bal,
+      });
+    }
+    await db.transaction().execute(async (trx) => {
+      await deleteUserBalances(user.userAddress, trx);
+      await insertUserBalances(newBalances, trx);
+    });
 
-		console.log(`Fixed ${user.userAddress} balances`);
-	}
+    console.log(`Fixed ${user.userAddress} balances`);
+  }
 }
