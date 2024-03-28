4cc23b8201347192153496f05757133fa1e42b36
not-reed <reed@degen.watch>
2024-03-28 08:20:06 -0700

    mobile light mode

--- apps/dapp/src/components/Layout.vue
+++ apps/dapp/src/components/Layout.vue
@@ -16,13 +16,15 @@ import TopBar from './TopBar.vue';
         </div>
 
     </div>
-    <div class="fixed -bottom-1 bg-calypso-700 left-0 right-0 md:hidden h-8 border-t border-calypso-900">
+    <div
+        class="fixed -bottom-1 bg-zinc-100 dark:bg-calypso-700 left-0 right-0 md:hidden h-8 border-t border-calypso-900">
         <ul class="flex justify-around text-calypso-800">
             <li>
                 <RouterLink to="/">
-                    <div class="bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
+                    <div
+                        class="bg-zinc-100 dark:bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
                         <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 82.2 93.2"
-                            class="w-6 h-6">
+                            class="w-6 h-6 p-px -top-px relative">
                             <path fill="currentColor"
                                 d="M0 93.2 41 0l41.2 93.1H27.6l6.2-14 28.2.1-21-47.8-27.2 61.8z" />
                         </svg>
@@ -31,21 +33,24 @@ import TopBar from './TopBar.vue';
             </li>
             <li>
                 <RouterLink to="/portfolio/overview">
-                    <div class="bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
+                    <div
+                        class="bg-zinc-100 dark:bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
                         <HomeIcon class="w-6 h-6" />
                     </div>
                 </RouterLink>
             </li>
             <li>
                 <RouterLink to="/tokens/listed">
-                    <div class="bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
+                    <div
+                        class="bg-zinc-100 dark:bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
                         <CurrencyDollarIcon class="w-6 h-6" />
                     </div>
                 </RouterLink>
             </li>
             <li>
                 <RouterLink to="/nfts/listed">
-                    <div class="bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
+                    <div
+                        class="bg-zinc-100 dark:bg-calypso-700 rounded-full p-2 relative -top-2 border-t border-calypso-900">
                         <PhotoIcon class="w-6 h-6" />
                     </div>
                 </RouterLink>
